c98b02108d220a30d0a52ece95799eab7d8de518
Mihail <siljanovskimicho@gmail.com>
2024-06-04 18:33:44 +0200

    created toolbar menu, activity

--- app/src/main/java/com/example/woofwoofbitola/MainActivity.java
+++ app/src/main/java/com/example/woofwoofbitola/MainActivity.java
@@ -1,24 +1,83 @@
 package com.example.woofwoofbitola;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+    private Button button1;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        if(drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityAllDogs();
+            }
+        });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.about){
+                    Toast.makeText(MainActivity.this, "About Selected", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.login) {
+                    Toast.makeText(MainActivity.this, "Login Selected", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.mkd){
+                    Toast.makeText(MainActivity.this, "Macedonian Selected", Toast.LENGTH_SHORT).show();
+                } else if (item.getItemId() == R.id.eng){
+                    Toast.makeText(MainActivity.this, "English Selected", Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void openActivityAllDogs(){
+        Intent intent = new Intent(this, ActivityAllDogs.class);
+        startActivity(intent);
     }
 }
